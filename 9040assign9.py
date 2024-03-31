87c46f2642e6c4ed78e29dc5992a6601b0ac4e1a
rplat1 <150953390+rplat1@users.noreply.github.com>
2024-03-31 14:33:58 -0400

    Add files via upload

---
+++ 9040assign9.py
@@ -0,0 +1,11 @@
+
+import math
+
+def calculate_volume(radius):
+    volume = (4/3) * 3.14 * radius**3
+    return volume
+
+radius = 5
+sphere_volume = calculate_volume(radius)
+
+print("The volume of a sphere with radius", radius, "is:", "{:.1f}".format(sphere_volume))
